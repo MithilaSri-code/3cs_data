203bbeb3c2ef917984c255062a5956210793cff8
Mithila Sriyanjana <mithila.sriyanjana@gmail.com>
2024-03-14 21:32:09 +0530

    backend data8

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("/path/to/node_modules/json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
